3a9409e50003320318c26167a6f860ccc3171b02
Patrick Murphy <patrick@patrickmurphyphoto.com>
2020-02-25 19:57:35 -0800

    Move Grammar to sub Folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var shell = require('shelljs');
 
 function defaultTask(cb) {
-	if (shell.exec('nearleyc .\\psych_script.ne -o .\\psych_script_compiled.js').code !== 0) {
+	if (shell.exec('nearleyc .\\grammar\\psych_script.ne -o .\\psych_script_compiled.js').code !== 0) {
 		shell.echo('Error: Browserify Command failed');
 		shell.exit(1);
 	}
@@ -11,23 +11,28 @@ function defaultTask(cb) {
 		shell.exit(1);
 	}
 	
-	if (shell.exec('nearley-railroad .\\psych_script.ne -o .\\docs\\psych_railroad.html').code !== 0) {
-		shell.echo('Error: railroad Command failed');
+	if (shell.exec('nearley-railroad .\\grammar\\psych_script.ne -o .\\docs\\psych_railroad.html').code !== 0) {
+		shell.echo('Error: railroad psych_script Command failed');
 		shell.exit(1);
 	}
 
-	if (shell.exec('nearley-railroad .\\psych_speech.ne -o .\\docs\\psych_speech_railroad.html').code !== 0) {
-		shell.echo('Error: railroad Command failed');
+	if (shell.exec('nearley-railroad .\\grammar\\psych_case_discovery.ne -o .\\docs\\psych_case_discovery_railroad.html').code !== 0) {
+		shell.echo('Error: railroad psych_script Command failed');
 		shell.exit(1);
 	}
 
-	if (shell.exec('nearley-railroad .\\psych_settings.ne -o .\\docs\\psych_settings_railroad.html').code !== 0) {
-		shell.echo('Error: railroad Command failed');
+	if (shell.exec('nearley-railroad .\\grammar\\psych_speech.ne -o .\\docs\\psych_speech_railroad.html').code !== 0) {
+		shell.echo('Error: railroad psych_speech Command failed');
+		shell.exit(1);
+	}
+
+	if (shell.exec('nearley-railroad .\\grammar\\psych_settings.ne -o .\\docs\\psych_settings_railroad.html').code !== 0) {
+		shell.echo('Error: railroad psych_settings Command failed');
 		shell.exit(1);
 	}
 	
-	if (shell.exec('nearley-railroad .\\psych_characters.ne -o .\\docs\\psych_characters_railroad.html').code !== 0) {
-		shell.echo('Error: railroad Command failed');
+	if (shell.exec('nearley-railroad .\\grammar\\psych_characters.ne -o .\\docs\\psych_characters_railroad.html').code !== 0) {
+		shell.echo('Error: railroad psych_characters Command failed');
 		shell.exit(1);
 	}
 
